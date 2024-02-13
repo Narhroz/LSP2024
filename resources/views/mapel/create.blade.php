38c0058a087d4ce48e14b41431bd3ee7d1f1203a
TokaTaka <110289565+Narhroz@users.noreply.github.com>
2024-02-13 20:34:16 +0700

    Mapel

---
+++ resources/views/mapel/create.blade.php
@@ -0,0 +1,21 @@
+@extends('layout.main')
+
+@section('content')
+    <div class="container-form">
+        <h2 align="center">Tambah Data Mapel</h2>
+
+        @if ($errors->any())
+            @foreach ($errors->all() as $error)
+                <p class="text-danger">{{ $error }}</p>
+            @endforeach
+        @endif
+
+        <form action="/mapel/store" method="post">
+            @csrf
+            <label for="nama_mapel">Mata Pelajaran</label>
+            <input type="text" name="nama_mapel" id="nama_mapel">
+
+            <button type="submit" class="button-submit">Simpan</button>
+        </form>
+    </div>
+@endsection
--- resources/views/mapel/index.blade.php
+++ resources/views/mapel/index.blade.php
@@ -1,5 +1,37 @@
 @extends('layout.main')
 
 @section('content')
-    //tes
+    <center>
+        <b>
+            <h2>LIST DATA MAPEL</h2>
+            <a href="/mapel/create" class="button-primary">TAMBAH DATA</a>
+            @if (session('success'))
+                <div class="alert alert-success"><span class="closebtn" id="closebtn">&times;</span>{{ session('success') }}</div>
+            @endif
+            @if (session('error'))
+                <div class="alert alert-danger"><span class="closebtn" id="closebtn">&times;</span>{{ session('error') }}</div>
+            @endif
+            <table class="tabel-data">
+                <thead>
+                    <tr>
+                        <th>NO</th>
+                        <th>MATA PELAJARAN</th>
+                        <th>ACTION</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach ($mapel as $m)
+                        <tr>
+                            <td>{{ $loop->iteration }}</td>
+                            <td>{{ $m->nama_mapel }}</td>
+                            <td style="text-align: center">
+                                <a href="/mapel/edit/{{ $m->id }}" class="button-warning">EDIT</a>
+                                <a href="/mapel/destroy/{{ $m->id }}" onclick="return confirm('Yakin Hapus?')" class="button-danger">HAPUS</a>
+                            </td>
+                        </tr>
+                    @endforeach
+                </tbody>
+            </table>
+        </b>
+    </center>
 @endsection
